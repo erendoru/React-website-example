9d655ad847500c747a606c6b2f45745b696addd1
Eren Doru <erendoru@hotmail.com>
2020-09-27 07:14:36 +0300

    Components are done
     pretty much important sections are done. need to finish the pages later.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Home from "./components/pages/Home";
+import Services from "./components/pages/Services";
+import Products from "./components/pages/Products";
+import SignUp from "./components/pages/SignUp";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -9,8 +14,12 @@ function App() {
       <Router>
         <Navbar />
         <Switch>
-          <Router path="/" exact />
+          <Route path="/" exact component={Home} />
+          <Route path="/services" component={Services} />
+          <Route path="/products" component={Products} />
+          <Route path="/sign-up" component={SignUp} />
         </Switch>
+        <Footer />
       </Router>
     </>
   );
